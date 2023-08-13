9644eb57392fcb709d89048cbdb75505e4f9a3e3
Artem <artem00561@gmail.com>
2023-08-13 10:24:54 +0300

    uvicorn update

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,12 +1,4 @@
-import uvicorn
-from uvicorn.config import LOGGING_CONFIG
+from subprocess import run
 
 
-LOGGING_CONFIG["formatters"]["access"]["fmt"] = '%(asctime)s %(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s'
-if __name__ == "__main__":
-    uvicorn.run(
-        "backend.wsgi:application",
-        host="0.0.0.0",
-        port=5000,
-        reload=True,
-    )
+run("gunicorn -w 1 -b 0.0.0.0:5000 backend.wsgi:application".split(' '))

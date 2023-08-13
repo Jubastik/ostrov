ddcab53277693348961647c7d6a53924ab5d3557
Artem <artem00561@gmail.com>
2023-08-13 19:07:38 +0300

    update

--- backend/backend/apps/weather/models.py
+++ backend/backend/apps/weather/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Weather(models.Model):
     camp = models.ForeignKey('camp.Camp', on_delete=models.CASCADE)
-    temperature = models.IntegerField()
-    humidity = models.IntegerField()
-    pressure = models.IntegerField()
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    pressure = models.FloatField()
     time = models.DateTimeField(auto_now_add=True)
